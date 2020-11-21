45d72eef5ca310d2d88c26b0ad417d5b19723968
chregger <chregger@gmx.at>
2020-11-21 13:11:24 +0100

    Remove unnecessary 2nd project

--- Handelsplattform_Gruppe5/handelsplattform/IEGEasyCreditCardRobin/Models/Log.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace IEGEasyCreditCardRobin.Models
-{
-    public class Log
-    {
-        public string Value { get; set; }
-        public Log(string value){ this.Value = value; }
-    }
-}
